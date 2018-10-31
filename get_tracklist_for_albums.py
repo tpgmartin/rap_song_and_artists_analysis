d479c898fb77814bf3ddc571614440d85d612cc8
tpgmartin <tpgmartin@gmail.com>
2018-10-31 16:47:05 +0000

    Get raw list of tracks by album and artist

--- get_tracklist_for_albums.py
+++ get_tracklist_for_albums.py
@@ -3,10 +3,6 @@ import csv
 import re
 import requests
 
-# Need to handle artists which are
-# * Various artists
-# 
-
 tracks_by_album_and_artist = []
 with open('./data/charting_rap_albums_since_2017.csv') as f:
     reader = csv.reader(f)
@@ -14,33 +10,33 @@ with open('./data/charting_rap_albums_since_2017.csv') as f:
     
     for row in reader:
 
-        artist = ('-').join(re.sub('[^0-9a-zA-Z\?\!]+',' ',row[0]).strip().split(' '))
-        album = ('-').join(re.sub('[^0-9a-zA-Z\?\!]+',' ',row[1]).strip().split(' '))
+        artist = row[0]
+        album = row[1]
 
-        print("artist", artist)
-        print("album", album)
+        formatted_artist = ('-').join(re.sub('[^0-9a-zA-Z\?\!]+',' ',artist).strip().split(' '))
+        formatted_album = ('-').join(re.sub('[^0-9a-zA-Z\?\!]+',' ',album).strip().split(' '))
 
-        url = "https://genius.com/albums/{}/{}".format(artist, album)
-
-        page = requests.get(url)
-        html = BeautifulSoup(page.text, "html5lib")
+        url = "https://genius.com/albums/{}/{}".format(formatted_artist, formatted_album)
 
         tracks = []
-        for el in html.findAll("h3", {"class": "chart_row-content-title"}):
+        response = requests.get(url)
+        if response.status_code == 404:
             track = []
             track.append(artist)
             track.append(album)
-            track.append(el.find(text=True).strip())
             tracks.append(track)
-        print('tracks')
-        print(tracks)
-        print('-------------------------')
+        else:
+            html = BeautifulSoup(response.text, "html5lib")
+            for el in html.findAll("h3", {"class": "chart_row-content-title"}):
+                track = []
+                track.append(artist)
+                track.append(album)
+                track.append(el.find(text=True).strip())
+                tracks.append(track)
         tracks_by_album_and_artist.extend(tracks)
-        print('tracks_by_album_and_artist')
-        print(tracks_by_album_and_artist)
-        print('--------------------------')
 
 with open("./data/tracks_by_artist_and_album.csv", "w") as f:
     w = csv.writer(f)
     w.writerow(["artist", "album", "track"])
-    w.writerow([track for track in tracks_by_album_and_artist])
+    for track in tracks_by_album_and_artist:
+        w.writerow(track)
