93c14218aec2606afebe440020bdd35cc10dbc5f
tpgmartin <tpgmartin@gmail.com>
2018-11-22 11:38:28 +0000

    Get genre from spotify api

---
+++ scripts/get_spotify_genres.py
@@ -0,0 +1,34 @@
+import config
+import csv
+import requests
+import pandas as pd
+
+base_url = "https://api.spotify.com/v1/search"
+
+def get_genres_for_artist(artist):
+
+    querystring = {
+        "q": artist,
+        "type": "artist"
+    }
+
+    headers = { 'Authorization': "Bearer {}".format(config.SPOTIFY_CONFIG["BEARER_TOKEN"]) }
+
+    response = requests.request("GET", base_url, headers=headers, params=querystring)
+
+    return response.json()["artists"]["items"][0]["genres"]
+
+if __name__ == "__main__":
+
+    artists = pd.read_csv("./data/target_artists.csv")["artist"]
+
+    artists_by_genres = []
+    for artist in artists:
+        genres_for_artist = get_genres_for_artist(artist)
+        artists_by_genres.append((artist, genres_for_artist))
+
+    with open("./data/artists_by_spotify_genres.csv", "w") as f:
+        w = csv.writer(f)
+        w.writerow(["artist", "genres"])
+        for row in artists_by_genres:
+            w.writerow([row[0], row[1]])
